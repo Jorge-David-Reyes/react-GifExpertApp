590fa4f0fa9541a66bcd58b16455ef0946258cd0
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-01-25 16:04:40 -0600

    76 - Creando una lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+
+  /* Hook listado de categorias */
+  const [categories, setCategories] = useState(['One Punch', 'Dragon ball']);
+
   return (
     <>
         {/* titulo */}
@@ -7,8 +13,15 @@ export const GifExpertApp = () => {
         {/* Input */}
 
         {/* Listado de Gif */}
+        <ol>
+          {
+            categories.map( category => {
+              return <li key={ category }>{category}</li>
+            })
+          }
+        </ol>
           {/* Gif Item */}
-          
+
     </>
   )
 }
