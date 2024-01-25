bc5cb02a2e36b6c77866d85dd390060c537ebeb8
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-01-25 15:44:16 -0600

    75 - GifExpertApp - Component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,14 @@
 export const GifExpertApp = () => {
   return (
     <>
+        {/* titulo */}
         <h1>GifExpertApp</h1>
+
+        {/* Input */}
+
+        {/* Listado de Gif */}
+          {/* Gif Item */}
+          
     </>
   )
 }
