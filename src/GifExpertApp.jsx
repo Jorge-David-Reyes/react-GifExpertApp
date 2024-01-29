50b33072d50d974d790450c217d0572aca77a3f3
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-01-29 15:13:40 -0600

    90 - Archivos de barril

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components";
 
 export const GifExpertApp = () => {
 
