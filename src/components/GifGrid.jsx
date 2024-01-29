6b9bee7d08549d343f8e718ab1cd88aaa01c94b1
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-01-28 18:21:24 -0600

    86 - Mostrar los titulos de las imagenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -3,14 +3,45 @@ import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
+
     /* @param deps — If present, effect will only activate if the values in the list change. */
-    useEffect(() => {
+    /*useEffect(() => {
         getGifs(category);
+    }, []);*/
+
+    /* Primera forma */
+    /*useEffect( () => {
+        getGifs(category)
+            .then( newImages => setImages(newImages) );
+    })*/
+
+    /* Segunda forma */
+    const getImages = async() => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    };
+
+    useEffect( () => {
+        getImages();
     }, []);
 
     return (
         <>
             <h3>{ category }</h3>
+
+            <ol>
+                {
+                    /*
+                    images.map( ({id, title}) => {
+                        return <li key={ id }>{ title }</li>
+                    })*/
+
+                    images.map( ({id, title}) => (
+                        <li key={ id }>{ title }</li>
+                    ))
+                }
+            </ol>
         </>
     )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,14 +1,14 @@
-export const getGifs = async(category) => {
+export const getGifs = async( category ) => {
 
     const url = `https://api.giphy.com/v1/gifs/search?api_key=yX8PtfuemVMGn5Zxdpo4EjTW6W7Ytxma&q=${ category }&limit=10`;
     const resp = await fetch( url );
-    const { data } = await  resp.json();
+    const { data } = await resp.json();
 
     const gifs = data.map( img => ({
-        id: img.ig,
-        title: img.tilte,
+        id: img.id,
+        title: img.title,
         url: img.images.downsized_medium.url
     }));
-
+    
     return gifs;
 }
