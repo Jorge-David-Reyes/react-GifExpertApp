eea299265a8dc4507753edfd083eb125c05c0e19
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-01-29 14:58:57 -0600

    89 - Mostrar mensaje de carga

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -15,6 +15,9 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3>{ category }</h3>
+            {
+                isLoading && ( <h2>Cargando...</h2>)
+            }
 
             <div className="card-grid">
                 {
